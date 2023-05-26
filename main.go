e5238cd1a4bc8a3fa66d17a90d1303164b36a2bf
edp1096 <edp1096@naver.com>
2023-05-26 20:47:38 +0900

    euclidian

--- main.go
+++ main.go
@@ -6,26 +6,66 @@ import (
 	"math/rand"
 )
 
-func main() {
-	f := 40
-	t := annoyindex.NewAnnoyIndexAngular(f)
+func angular() {
+	dimension := 40
+	indexing := annoyindex.NewAnnoyIndexAngular(dimension)
 	for i := 0; i < 1000; i++ {
-		item := make([]float32, 0, f)
-		for x := 0; x < f; x++ {
+		item := make([]float32, 0, dimension)
+		for x := 0; x < dimension; x++ {
 			item = append(item, rand.Float32())
 		}
-		t.AddItem(i, item)
+		indexing.AddItem(i, item)
 	}
-	t.Build(10)
-	t.Save("test.ann")
+	indexing.Build(10)
+	indexing.Save("test_ang.ann")
 
-	annoyindex.DeleteAnnoyIndexAngular(t)
+	annoyindex.DeleteAnnoyIndexAngular(indexing)
 
-	t = annoyindex.NewAnnoyIndexAngular(f)
-	t.Load("test.ann")
+	indexing = annoyindex.NewAnnoyIndexAngular(dimension)
+	indexing.Load("test_ang.ann")
 
 	var result []int
-	t.GetNnsByItem(0, 1000, -1, &result)
+	indexing.GetNnsByItem(0, 1000, -1, &result)
 	fmt.Printf("%v\n", result)
+}
 
+func euclidean() {
+	dimension := 3
+	indexing := annoyindex.NewAnnoyIndexEuclidean(dimension)
+
+	indexing.AddItem(0, []float32{1.2, 3.4, 5.6})
+	indexing.AddItem(1, []float32{2.1, 4.3, 6.5})
+	indexing.AddItem(2, []float32{0.8, 1.5, 2.7})
+	indexing.AddItem(3, []float32{3.2, 2.8, 1.4})
+
+	indexing.Build(10)
+	indexing.Save("test_euc.ann")
+
+	indexing.Unload()
+
+	var nearest []int
+
+	indexing = annoyindex.NewAnnoyIndexEuclidean(dimension)
+	indexing.Load("test_euc.ann")
+
+	query := []float32{0.9, 2.3, 4.5}
+	nNeighbors := 2
+
+	indexing.GetNnsByVector(query, nNeighbors, -1, &nearest)
+
+	fmt.Println("Query Vector:", query)
+	fmt.Println("Nearest Neighbors:", nearest)
+
+	var valueFirst, valueSecond []float32
+	indexing.GetItem(nearest[0], &valueFirst)
+	indexing.GetItem(nearest[1], &valueSecond)
+	fmt.Println(nearest[0], valueFirst)
+	fmt.Println(nearest[1], valueSecond)
+	dist := indexing.GetDistance(nearest[0], nearest[1])
+	fmt.Println("Distance between nearest 1st and 2nd:", dist)
+}
+
+func main() {
+	angular()
+	euclidean()
 }
